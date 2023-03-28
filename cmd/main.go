fba3d7964f7992d58bc3a03b4d4ecf0e4b791625
Levi Lovelock <levi@Levis-MacBook-Pro.local>
2023-03-27 22:33:20 -0700

    first commit for glocks!!

---
+++ cmd/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"errors"
+	"github.com/levpaul/glocks/internal/interpreter"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+	"os"
+)
+
+func main() {
+	execute()
+}
+
+func execute() {
+	logCfg := zap.NewDevelopmentConfig()
+	logCfg.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
+	logCfg.DisableStacktrace = true
+	logCfg.DisableCaller = true
+	logCfg.EncoderConfig.TimeKey = "" // Disable timestamps
+
+	rawLogger, _ := logCfg.Build()
+	defer rawLogger.Sync() // flushes buffer, if any
+	log := rawLogger.Sugar()
+
+	glocksI := interpreter.New(log)
+
+	var rootCmd = &cobra.Command{
+		Use:           "glocks",
+		Short:         "glocks <file> run <file> or open the glocks REPL",
+		SilenceUsage:  true,
+		SilenceErrors: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) > 1 {
+				log.Error("Expected maximum of 1 arg - received '", args, "' - exiting 1")
+				return errors.New("too many args")
+			}
+
+			if len(args) == 1 {
+				return glocksI.RunFile(args[0])
+			}
+
+			return glocksI.REPL()
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		// Cobra logic is expected to print human friendly error
+		log.Debug("error code: ", err.Error())
+		os.Exit(1)
+	}
+
+}
