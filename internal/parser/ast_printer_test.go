c8963a6c18721e23cc9bf66c7c6ee9527531a411
Levi Lovelock <lovelock.levi@gmail.com>
2025-02-08 11:49:45 -0800

    Fix AST test

--- internal/parser/ast_printer_test.go
+++ internal/parser/ast_printer_test.go
@@ -1,22 +1,23 @@
 package parser
 
 import (
+	"testing"
+
 	"github.com/levpaul/glocks/internal/lexer"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 func TestPrintExpression(t *testing.T) {
 	// Create -123 * (45.67) as an Expression (tree) and print it
 	expr := Binary{
-		Left: Unary{
+		Left: &Unary{
 			Operator: &lexer.Token{
 				Type:    lexer.MINUS,
 				Lexeme:  "-",
 				Literal: nil,
 				Line:    1,
 			},
-			Right: Literal{Value: 123},
+			Right: &Literal{Value: 123},
 		},
 		Operator: &lexer.Token{
 			Type:    lexer.STAR,
@@ -24,9 +25,9 @@ func TestPrintExpression(t *testing.T) {
 			Literal: nil,
 			Line:    1,
 		},
-		Right: Grouping{Expression: Literal{Value: 45.67}},
+		Right: &Grouping{Expression: &Literal{Value: 45.67}},
 	}
 
 	walker := ExprPrinter{}
-	assert.Equal(t, "(* (- 123) (group 45.67))", walker.Print(expr))
+	assert.Equal(t, "(* (- 123) (group 45.67))", walker.Print(&expr))
 }
